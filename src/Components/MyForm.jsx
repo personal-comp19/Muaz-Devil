736a95fa1162b12c8feed219365eb1bc865c2ed2
Muaz  Zafar <personalcomp1998@gmail.com>
2024-08-16 14:34:48 +0500

    Update MyForm.jsx

--- src/Components/MyForm.jsx
+++ src/Components/MyForm.jsx
@@ -26,65 +26,73 @@ function MyForm() {
 
   return (
     <div className='Form'>
-      <h1>HTML Form</h1>
-      <form onSubmit={handleSubmit}>
-        <label>
-          First Name:
-          <input
-            type="text"
-            value={firstName}
-            placeholder='Enter your first name'
-            onChange={(e) => setFirstName(e.target.value)}
-          />
-        </label>
-        <br />
-        <label>
-          Last Name:
-          <input
-            type="text"
-            value={lastName}
-           placeholder='Enter your last name'
-            onChange={(e) => setLastName(e.target.value)}
-          />
-        </label>
-        <br />
-        <label>
-          Date of Birth:
-          <input
-            type="date"
-            value={dob}
-            onChange={(e) => setDob(e.target.value)}
-          />
-        </label>
-        <br />
-        <label>
-          Email:
-          <input
-            type="email"
-            value={email}
-            placeholder='Enter your email ID'
-            onChange={(e) => setEmail(e.target.value)}
-          />
-        </label>
-        <br />
-        <label>
-          Mobile Number:
-          <input
-            type="tel"
-            value={mobile}
-            placeholder='Enter your Mobile Number'
-            onChange={(e) => setMobile(e.target.value)}
-          />
-        </label>
-        <br />
-        <button type="submit">SUBMIT</button>
-        <button type="button" onClick={handleReset}>
-          RESET
-        </button>
-      </form>
-       {submitted && <p className="success-message">Form has been submitted!</p>}
-
-
+      {!submitted ? (
+        <>
+          
+          <form onSubmit={handleSubmit}>
+            <label>
+              First Name:
+              <input
+                type="text"
+                value={firstName}
+                placeholder='Enter your first name'
+                onChange={(e) => setFirstName(e.target.value)}
+              />
+            </label>
+            <br />
+            <label>
+              Last Name:
+              <input
+                type="text"
+                value={lastName}
+                placeholder='Enter your last name'
+                onChange={(e) => setLastName(e.target.value)}
+              />
+            </label>
+            <br />
+            <label>
+              Date of Birth:
+              <input
+                type="date"
+                value={dob}
+                onChange={(e) => setDob(e.target.value)}
+              />
+            </label>
+            <br />
+            <label>
+              Email:
+              <input
+                type="email"
+                value={email}
+                placeholder='Enter your email ID'
+                onChange={(e) => setEmail(e.target.value)}
+              />
+            </label>
+            <br />
+            <label>
+              Mobile Number:
+              <input
+                type="tel"
+                value={mobile}
+                placeholder='Enter your Mobile Number'
+                onChange={(e) => setMobile(e.target.value)}
+              />
+            </label>
+            <br />
+            <button type="submit">SUBMIT</button>
+            <button type="button" onClick={handleReset}>
+              RESET
+            </button>
+          </form>
+        </>
+      ) : (
+        <>
+          <p className="success-message">Form has been submitted successfully!</p>
+          <button type="button" onClick={handleReset}>
+            GO BACK
+          </button>
+        </>
+      )}
     </div>
   );
 }
